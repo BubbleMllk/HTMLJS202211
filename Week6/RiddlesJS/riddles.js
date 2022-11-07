558d960c934364df069273e0a5335cf089c6d2db
Terry Yang <tyang@email.neit.edu>
2022-11-07 13:59:16 -0500

    Week 6 day 1

---
+++ Week6/RiddlesJS/riddles.js
@@ -0,0 +1,8 @@
+function showAnswer(){
+    document.getElementById("answer").innerHTML = "The moon!"
+
+}
+
+function showAnswer2(){
+    document.getElementById("answer2").innerHTML = "The dark."
+}
