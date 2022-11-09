e1116d9b2f4baf6fbbb4c74051355c1768e86feb
Terry Yang <tyang@email.neit.edu>
2022-11-09 11:10:40 -0500

    Shape excercise hw

--- Week4 CSSPositionAndContainers/d1_assignment/script.js
+++ Week4 CSSPositionAndContainers/d1_assignment/script.js
@@ -1,29 +1,68 @@
 var canvas = document.getElementById("canvas")
 
 //Defines the drawing context of the Canvas Element
-var con = canvas.getContext('2d')
+var ctx = canvas.getContext('2d')
 
 //Sets up color and outline styles
-con.fillStyle = "rgb(0,0,255)"
-con.strokeStyle = "red"
-
+ctx.fillStyle = "yellow"
+ctx.strokeStyle = "black"
+ctx.lineWidth = 5;
 
 //Draws a square fillRect(x,y,width,height)
-con.fillRect(86, 302, 100, 102)
-con.strokeRect(86, 302, 100, 102)
-
-//Draw lines
-con.beginPath()
-con.moveTo(86, 302)
-con.lineTo(86, 302)
-con.lineTo(86, 302)
-con.stroke()
+ctx.fillRect(84, 302, 107, 107)
+ctx.strokeRect(84, 302, 107, 107)
 
 
-con.strokeStyle = "red"
+//Draws a circle
 
-con.beginPath()
-con.moveTo(800,600)
-con.lineTo(400,350)
-con.lineTo(0,600)
-con.stroke()
+ctx.strokeStyle = "red"
+ctx.fillStyle = "#ffff00"
+
+ctx.beginPath();
+ctx.arc(385,441,68,0*Math.PI,2*Math.PI)
+ctx.closePath();
+ctx.fill();
+ctx.stroke(); 
+
+//Draw a pentagon
+ctx.fillStyle = "#ff00ff"
+ctx.strokeStyle = "#00ffff"
+
+ctx.beginPath()
+ctx.moveTo(557,308);
+ctx.lineTo(670,284);
+ctx.lineTo(726,380);
+ctx.lineTo(655,470);
+ctx.lineTo(543,419);
+ctx.closePath();
+ctx.fill();
+ctx.stroke();
+
+//Drawing a star
+ctx.fillStyle = "#ffff00"
+ctx.strokeStyle = "rgb(32,32,32)"
+
+ctx.beginPath()
+ctx.moveTo(636,497)
+ctx.lineTo(668,554)
+ctx.lineTo(732,567)
+ctx.lineTo(688,615)
+ctx.lineTo(695,680)
+ctx.lineTo(636,653)
+ctx.lineTo(575,681)
+ctx.lineTo(586,615)
+ctx.lineTo(538,567)
+ctx.lineTo(603,554)
+ctx.lineTo(636,497)
+ctx.closePath()
+ctx.fill()
+ctx.stroke()
+
+//Draw a line
+ctx.strokeStyle = "rgb(25,0,0)"
+
+ctx.beginPath()
+ctx.moveTo(85,682)
+ctx.lineTo(278,549)
+ctx.closePath()
+ctx.stroke()
