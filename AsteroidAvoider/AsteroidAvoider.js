e9084d34f459d778d60fc2f6ec907bdd763556a7
Terry Yang <tyang@email.neit.edu>
2022-12-07 13:23:06 -0500

    End of class Week 8 day 2

--- AsteroidAvoider/AsteroidAvoider.js
+++ AsteroidAvoider/AsteroidAvoider.js
@@ -1,9 +1,12 @@
 var canvas = document.getElementById("canvas")
 var ctx = canvas.getContext("2d")
 var timer = requestAnimationFrame(main)
-var gameOVer = false
+var gameOVer = true
 var score = 0
 var Highscore = 0
+var currentstate = 0
+var gameState = []
+
 
 //Asteroid variables
 var numAsteroids = 20
@@ -40,13 +43,40 @@ function pressKeyDown(e) {
         }
     }
 
+    //Menu inputs use spacebar
+if(gameOVer){
+    if(e.keyCode == 32){
+        if(currentstate == 2){
+            //gameoverinput
+            currentstate = 0
+            numAsteroids = 20
+            asteroids = []
+            score = 0
+            //start game here
+            main()
+           // gameStart()
+            
 
+
+        }else{
+            //main menu input
+            gameStart()
+            currentstate = 1
+            gameOVer = false
+            main()
+            scoreTimer()
+            
+        }
+
+
+    }
+}
 
 }
 
 function pressKeyUp(e) {
     if (!gameOVer) {
-    }
+    
 
     if (e.keyCode == 87) {
         //code for forward key W
@@ -68,6 +98,9 @@ function pressKeyUp(e) {
         ship.down = false
 
     }
+
+}
+
 
 }
 
@@ -107,10 +140,33 @@ function PlayerShip() {
     this.right = false
     this.vy = 0
     this.vx = 0
+    this.flamelength = 30
 
     this.drawShip = function () {
         ctx.save()
         ctx.translate(this.x, this.y)
+
+        //draw the thruster
+        if(this.up || this.left || this.right){
+            ctx.save()
+            if(this.flamelength == 30){
+                this.flamelength = 20
+                ctx.fillStyle = "yellow"
+
+            }else{
+                this.flamelength = 30
+                ctx.fillStyle = "orange"
+            }
+            //draw the flame
+            ctx.beginPath()
+            ctx.moveTo(0,this.flamelength)
+            ctx.lineTo(5,5)
+            ctx.lineTo(-5,5)
+            ctx.lineTo(0, this.flamelength)
+            ctx.closePath()
+            ctx.fill()
+            ctx.restore()
+        }
 
         //draw the ship
         ctx.fillStyle = "Red"
@@ -159,18 +215,40 @@ function PlayerShip() {
 
 }
 
-//for loop to instantiate asteroids for game
-for (var i = 0; i < numAsteroids; i++) {
-    asteroids[i] = new Asteroid()
-
-}
-
-
 //FUNCTION MAIN -------------------------------------------------------
 function main() {
     //clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
+    gameState[currentstate]()
+
+    if (!gameOVer) {
+        //refresh the screen
+        timer = requestAnimationFrame(main)
+    }
+
+
+
+}
+
+//Game state machine------------------------------------------------------
+
+//Main menu state
+gameState[0] = function () {
+    //code for main menu
+    ctx.save()
+    ctx.font = "30px Arial"
+    ctx.fillStyle = "white"
+    ctx.textAlign = "center"
+    ctx.fillText("Asteroid Avoider", canvas.width / 2, canvas.height / 2 - 30)
+    ctx.font = "15px Arial"
+    ctx.fillText("Press space to start", canvas.width / 2, canvas.height / 2 + 20)
+    ctx.restore()
+}
+
+//play game state
+gameState[1] = function () {
+    //code for the asteroid game
     //draw score to screen
     ctx.save()
     ctx.font = "15px Arial"
@@ -204,10 +282,14 @@ function main() {
         //pythagorean theorem inserted here for the distance
         var distance = Math.sqrt((dX * dX) + (dY * dY))
 
+        //Collision detection happends here
         if (detectCollision(distance, (ship.height / 2 + asteroids[i].radius))) {
-           // console.log("Hit Asteroid")
-           // alert("Hit Asteroid")
-           gameOVer = true
+            // console.log("Hit Asteroid")
+            // alert("Hit Asteroid")
+            gameOVer = true
+            currentstate = 2
+            main()
+            return;
         }
 
 
@@ -223,13 +305,7 @@ function main() {
     //draw the ship
     ship.moveShip()
     ship.drawShip()
-
-    if(!gameOVer){
-        //refresh the screen
-        timer = requestAnimationFrame(main)  
-    }
-   
-    while(asteroids.length < numAsteroids){
+    while (asteroids.length < numAsteroids) {
         //creates new asteroid
         asteroids.push(new Asteroid())
 
@@ -237,9 +313,50 @@ function main() {
 
 }
 
+//Game over state
+gameState[2] = function () {
+    if(score> Highscore){
+        Highscore = score
 
+            //code for game over menu
+    ctx.save()
+    ctx.font = "30px Arial"
+    ctx.fillStyle = "white"
+    ctx.textAlign = "center"
+    ctx.fillText("Game Over, Your score was: " + score.toString(), canvas.width/2, canvas.height/2 - 60)
+    ctx.fillText("Your New High Score is: " + Highscore.toString(), canvas.width/2, canvas.height/2 - 30)
+    ctx.fillText("New Record!" , canvas.width/2, canvas.height/2)
+    ctx.font = "15px Arial"
+    ctx.fillText("Press Space to play again", canvas.width/2, canvas.height/2 + 20)
+    ctx.restore()
+    }
+    else{
+    
+    ctx.save()
+    ctx.font = "30px Arial"
+    ctx.fillStyle = "white"
+    ctx.textAlign = "center"
+    ctx.fillText("Game Over, Your score was: " + score.toString(), canvas.width/2, canvas.height/2 - 60)
+    ctx.fillText("Your High Score is: " + Highscore.toString(), canvas.width/2, canvas.height/2 - 30)
+    ctx.font = "15px Arial"
+    ctx.fillText("Press Space to play again", canvas.width/2, canvas.height/2 + 20)
+    ctx.restore()
+    }
+
+
+}
 
 //utility functions
+function gameStart(){
+    //for loop to instantiate asteroids for game
+for (var i = 0; i < numAsteroids; i++) {
+    asteroids[i] = new Asteroid()
+
+}
+
+ship = new PlayerShip()
+}
+
 
 function randomRange(high, low) {
     return Math.random() * (high - low) + low
@@ -251,11 +368,11 @@ function detectCollision(distance, calcDistance) {
     return distance < calcDistance
 }
 
-function scoreTimer(){
-    if(!gameOVer){
+function scoreTimer() {
+    if (!gameOVer) {
         score++
 
-        if(score % 5 == 0){
+        if (score % 5 == 0) {
             numAsteroids += 5
             console.log(numAsteroids)
         }
@@ -265,5 +382,5 @@ function scoreTimer(){
     }
 }
 
-//temp call score function
-scoreTimer()
+// //temp call score function
+// scoreTimer()
